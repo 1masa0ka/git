8b1d80b7a93ce710c8195a83dc050cca94b3d22e
1masa0ka <93691707+1masa0ka@users.noreply.github.com>
2021-11-12 10:59:54 +0900

    Update git_in.py

--- git_in.py
+++ git_in.py
@@ -1,26 +1,16 @@
 import base64
 from github import Github
+import streamlit as st
 
-token = "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"
+token = "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"
 repository = "1masa0ka/CR"
 fileName = "CR_date.csv"
 
-def get_file():
+g = Github(token)
+repo = g.get_repo(repository)
+contents = repo.get_contents(fileName)
+content = base64.b64decode(contents.content)
 
-    g = Github(token)
-    repo = g.get_repo(repository)
-    contents = repo.get_contents(fileName)
-    content = base64.b64decode(contents.content)
-
-    with open("copy_" + fileName, mode="wb") as f:
-        f.write(content)
-
-    return("[succeed]")
-
-def main():
-        result = get_file()
-        print(result)
-
-if __name__ == "__main__":
-    main()
-
+st.write('git')
+with open("copy_" + fileName, mode="wb") as f:
+    f.write(content)
