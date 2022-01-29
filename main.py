86f290ab4435509e34e7b22f04b070c35cfd8ecc
1masa0ka <93691707+1masa0ka@users.noreply.github.com>
2022-01-29 23:08:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ st.title('クリーンルーム予約')
 st.write('荒木研　＠機械工学2号棟　309a室')
 st.subheader('現在の予約状況')
 
-pass=st.secrets["password"]
+pass_true=st.secrets["password"]
 pass_in = st.text_input('PASSWORD')
 
-if pass_in==pass:
+if pass_in==pass_true:
   st.write("OK")
  else:
   st.write("パスワードが違います")
