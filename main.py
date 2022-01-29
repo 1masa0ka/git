ac529ebd9ed60efdbaea58e338dcb0f4c76ee700
1masa0ka <93691707+1masa0ka@users.noreply.github.com>
2022-01-29 23:13:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import datetime
 import re
 import csv
 
-st.title('クリーンルーム予約')
-st.write('荒木研　＠機械工学2号棟　309a室')
-st.subheader('現在の予約状況')
+st.title('研究紹介')
+
 
 pass_true=st.secrets["password"]
 pass_in = st.text_input('PASSWORD')
+pass_st=0
 
 if pass_in==pass_true:
   st.write("OK")
+  pass_st=1
+elif pass_in=='':
+  st.write("パスワードを入力し、Enterキーを押してください")
 else:
   st.write("パスワードが違います")
 
+if pass_st==1:
+  st.write('荒木研　＠機械工学2号棟　309a室')
+  st.subheader('現在の予約状況')
 
