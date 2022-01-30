a6c17e6c6af3e5aed24cf6cadede2fc9436411c5
1masa0ka <93691707+1masa0ka@users.noreply.github.com>
2022-01-30 15:20:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ if pass_st==1:
       img = Image.open(img_bin)
       st.image(img, caption='Sunrise by the mountains')
     #テキスト表示
-    with zp.open('DATA/'+str(str(slides[0]))+'.txt',pwd=pass_in.encode("utf-8"), encoding='utf-8') as txt_file:
+    with zp.open('DATA/'+str(str(slides[0]))+'.txt',pwd=pass_in.encode("utf-8")) as txt_file:
       txtdata = txt_file.read()
       st.write(txtdata)
       
