c54e0751502ae7ec2ff01372dbdb855cf48b4232
1masa0ka <93691707+1masa0ka@users.noreply.github.com>
2022-01-30 14:36:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,20 +22,17 @@ else:
   st.write("パスワードが違います")
 
 if pass_st==1:
-  filename = "./DATA.zip/DATA"
+  filename = "./DATA.zip"
   with zipfile.ZipFile(filename, "r") as zp:
     for info in zp.infolist():
-        # 「zipファイル名/」については処理をしない    
-      with zp.open(info.filename,pwd=pass_in.encode("utf-8")) as img_file:
-        # 画像のバイナリデータを読み込む
-        img_bin = io.BytesIO(img_file.read())
-        # バイナリデータをpillowから開く
-        st.image(img_bin, caption='Sunrise by the mountains')
+      if (info.filename != 'DATA/'):
+        with zp.open(info.filename,pwd=pass_in.encode("utf-8")) as img_file:
+          img_bin = io.BytesIO(img_file.read())
+          img = Image.open(img_bin)
+          st.image(img, caption='Sunrise by the mountains')
     #zp.extractall(pwd=pass_in.encode("utf-8"))
     st.write("The extract is complete.")
         
   st.sidebar.write('テーマ選択')
-  
-  
 
   
