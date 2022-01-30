616d927e5d568b9a047b80e60f10f14ebc8869b7
1masa0ka <93691707+1masa0ka@users.noreply.github.com>
2022-01-30 14:54:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ if pass_st==1:
   with zipfile.ZipFile(filename, "r") as zp:
     for info in zp.infolist():
       if (info.filename != 'DATA/'):
-        if (info.filename == 'DATA/'+str(slides[0])+'.png')
+        if (info.filename == 'DATA/'+str(str(slides[0]))+'.png')
           with zp.open(info.filename,pwd=pass_in.encode("utf-8")) as img_file:
             img_bin = io.BytesIO(img_file.read())
             img = Image.open(img_bin)
