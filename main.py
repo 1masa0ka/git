8b9b792941ad58c0206fea067ada7f1396614924
1masa0ka <93691707+1masa0ka@users.noreply.github.com>
2022-01-31 21:04:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,13 +13,14 @@ pass_true=st.secrets["password"]
 pass_in = st.text_input('PASSWORD')
 pass_st=0
 
-if pass_in==pass_true:
-  #st.write("OK")
-  pass_st=1
-elif pass_in=='':
-  st.write("パスワードを入力し、Enterキーを押してください")
-else:
-  st.write("パスワードが違います")
+if pass_st==0:
+  if pass_in==pass_true:
+    #st.write("OK")
+    pass_st=1
+  elif pass_in=='':
+    st.write("パスワードを入力し、Enterキーを押してください")
+  else:
+    st.write("パスワードが違います")
 
 if pass_st==1:
   st.sidebar.write('表示')
